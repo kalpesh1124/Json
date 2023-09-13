3e9954c76968db106df21636ef8d784db786becb
kalpesh1124 <kalpeshinde170@gmail.com>
2023-09-14 00:41:45 +0530

    Initial commit

---
+++ JsonWebApplication2/JsonWebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using JsonWebApplication2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace JsonWebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        MvcDataEntities db = new MvcDataEntities();
+        // GET: Student
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        public JsonResult GetStudent()
+        {
+            List<stdinfo> temp = db.stdinfoes.ToList();
+            return Json(temp, JsonRequestBehavior.AllowGet);
+        }
+
+        public string AddStudent(stdinfo s)
+        {
+            db.stdinfoes.Add(s);
+            db.SaveChanges();
+            return "Student Added Successfully!!!!";
+        }
+
+        public JsonResult GetStudentById(int id)
+        {
+            stdinfo s = db.stdinfoes.Find(id);
+            return Json(s, JsonRequestBehavior.AllowGet);
+        }
+
+        public string UpdateStudent(stdinfo s)
+        {
+            db.Entry<stdinfo>(s).State = EntityState.Modified;
+            db.SaveChanges();
+            return "Student Updated Successfully!!!!";
+        }
+
+        public string DeleteStudent(int id)
+        {
+            stdinfo s = db.stdinfoes.Find(id);
+            db.stdinfoes.Remove(s);
+            db.SaveChanges();
+            return "Student Deleted Successfully!!!!";
+        }
+    }
+}
---
+++ JsonWebApplication2/JsonWebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsonWebApplication2.MvcApplication" Language="C#" %>
